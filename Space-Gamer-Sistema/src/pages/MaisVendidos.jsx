4e0fe33ddfa8e02abb36117589b12786cb1e13b2
Wagner Guedes <wagnerguedes123@hotmail.com>
2024-04-19 17:29:38 -0300

    update

--- Space-Gamer-Sistema/src/pages/MaisVendidos.jsx
+++ Space-Gamer-Sistema/src/pages/MaisVendidos.jsx
@@ -2,6 +2,10 @@ import React, { useState } from 'react';
 import './MaisVendidos.css'
 
 import estrela from '../images/estrela.png'
+import sensaçao from '../images/tacasensacao.png'
+
+
+
 const MaisVendidos = () => {
   
 
@@ -19,6 +23,18 @@ const MaisVendidos = () => {
                 <img src={estrela}/>
             </div>
         </li>
+
+        <li className='product'>
+            <div >
+            <h4>Taça sensação</h4>
+            <p className='info-product'>Açai,mousse de morango, chantilly, morango, leite em pó, gotas de chocolate e nutela.</p>
+            <p className='value-product'>300ml - R$ 23,70 <br/> 400ml - R$ 24,50 <br/> 500ml - R$ 26,50</p>
+            </div>
+            <div className='image'>
+                <img src={sensaçao}/>
+            </div>
+        </li>
+
         <li className='product'>
             <div >
             <h4>Estrela</h4>
@@ -29,16 +45,7 @@ const MaisVendidos = () => {
                 <img src={estrela}/>
             </div>
         </li>
-        <li className='product'>
-            <div >
-            <h4>Estrela</h4>
-            <p className='info-product'>Pão brioche, burguer artesanal de 90g, cheddar americano, fatias de bacon e molho barbecue.</p>
-            <p className='value-product'>R$ 23,00</p>
-            </div>
-            <div className='image'>
-                <img src={estrela}/>
-            </div>
-        </li>
+
         <li className='product'>
             <div >
             <h4>Estrela</h4>
